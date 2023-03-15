0c54c0f9e3d4028fc8eec8bdb4ee05a5e54383e0
KnightSorea <itsjunkratmain64@gmail.com>
2023-03-15 14:00:13 -0700

    mini pop up ad
    this will be a demonstration on how to play the game as a tutorial

---
+++ Assets/Danny/scripts/fakePlayerBeatTutorialLevel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fakePlayerBeatTutorialLevel : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

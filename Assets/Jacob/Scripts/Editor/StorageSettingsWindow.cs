2358ef73d970f24af685010f160cdd00fccd2d94
jnielsengamedev <jnielsengamedev@gmail.com>
2023-03-29 13:54:11 -0700

    Make a fully fledged Storage system.
    This commit makes a fully fledged storage system with a Custom Inspector. The Inspector has a preset system where you can either set the storage script to use a Gold Storage preset or a Elixir preset and theres a button that opens a custom Editor Window that lets you modify the preset settings so you can change the preset values to set different values if you don't feel like the defaults are what you want.

---
+++ Assets/Jacob/Scripts/Editor/StorageSettingsWindow.cs
@@ -0,0 +1,55 @@
+﻿using Jacob.Scripts.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jacob.Scripts.Editor
+{
+	public class StorageSettingsWindow : EditorWindow
+	{
+		public static void ShowWindow()
+		{
+			GetWindow<StorageSettingsWindow>("Storage Preset Settings");
+		}
+
+		private void OnGUI()
+		{
+			var storage = Resources.Load("StoragePresetSettings") as StoragePreset;
+			if (!storage)
+			{
+				storage = StoragePreset.CreateStoragePreset();
+			}
+
+			EditorGUILayout.LabelField("Gold Storage", EditorStyles.boldLabel);
+			storage.goldStorageAmount = EditorGUILayout.FloatField("Money to Generate",
+				storage.goldStorageAmount);
+			storage.goldStorageTime = EditorGUILayout.FloatField("Time between Generation",
+				storage.goldStorageTime);
+			EditorGUILayout.LabelField("Elixir Storage", EditorStyles.boldLabel);
+			storage.elixirAmount = EditorGUILayout.FloatField("Money to Generate",
+				storage.elixirAmount);
+			storage.elixirTime = EditorGUILayout.FloatField("Time between Generation",
+				storage.elixirTime);
+
+			if (GUI.changed)
+			{
+				EditorUtility.SetDirty(storage);
+			}
+
+			using (var _ = new EditorGUI.DisabledScope(!EditorUtility.IsDirty(storage)))
+			{
+				if (GUILayout.Button("Save"))
+				{
+					AssetDatabase.SaveAssetIfDirty(storage);
+					EditorUtility.ClearDirty(storage);
+				}
+			}
+
+			using (var _ = new EditorGUI.DisabledScope(StoragePreset.IsDefault(storage)))
+			{
+				if (!GUILayout.Button("Reset to Default")) return;
+				EditorUtility.SetDirty(storage);
+				StoragePreset.ResetToDefault(storage);
+			}
+		}
+	}
+}

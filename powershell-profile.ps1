4a1e83e13e13b2b5a934ebec5830c5c168ce2c00
jnielsengamedev <jnielsengamedev@gmail.com>
2023-04-11 12:41:07 -0700

    Add a Powershell profile with useful commands.

---
+++ powershell-profile.ps1
@@ -0,0 +1,51 @@
+function Fetch($branch) {
+	Switch($branch) {
+		"working" {
+			git fetch origin WorkingBranch:WorkingBranch
+		}
+		"jacob" {
+			git fetch origin JacobBranch:JacobBranch
+		}
+		"" {
+			Write-Output "Please provide a branch"
+		}
+		Default {
+			git fetch origin ${branch}:${branch}
+		}
+	}
+}
+
+function Rebase($branch) {
+	Switch($branch) {
+		"working" {
+			git rebase WorkingBranch
+		}
+		"" {
+			Write-Output "Please provide a branch"
+		}
+		Default {
+			git rebase ${branch}
+		}
+	}
+}
+
+function Checkout($branch) {
+	Switch($branch) {
+		"working" {
+			git checkout WorkingBranch
+		}
+		"jacob" {
+			git checkout JacobBranch
+		}
+		"" {
+			Write-Output "Please provide a branch"
+		}
+		Default {
+			git checkout ${branch}
+		}
+	}
+}
+
+function DeleteBranch($branch) {
+	git branch -d ${branch}
+}

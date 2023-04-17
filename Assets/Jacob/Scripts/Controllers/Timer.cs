b764ed98c68c26902a72dc752cbf884eb9577f65
jnielsengamedev <jnielsengamedev@gmail.com>
2023-04-17 13:35:38 -0700

    Make a Timer.
    This commit makes a Timer that counts down with an animation and it shows how many seconds are left in the clock. When the timer is out of time, it will fire off a Unity Event.
    
    This timer is manually animated by using a for loop and it waits around (1 / 17 fps) seconds per loop.
    
    I also unhooked the Collider2D dependency from the ActivateDeactivate script so you can put it on container GameObjects without requiring a Collider2D and I made the Activate and Deactivate methods public.

---
+++ Assets/Jacob/Scripts/Controllers/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Jacob.Scripts.Controllers
+{
+	public class Timer : MonoBehaviour
+	{
+		public int seconds;
+		public UnityEvent onOutOfTime;
+
+		private Animator _animator;
+		private TextMeshProUGUI _timeText;
+		private int _secondsLeft;
+		private float _frames;
+		private float _secondsPerFrame;
+
+		private void Awake()
+		{
+			_animator = GetComponent<Animator>();
+			_animator.speed = 0;
+			_timeText = transform.Find("Time").GetComponent<TextMeshProUGUI>();
+			GetFrames();
+			StartCoroutine(Animation());
+		}
+
+		private void GetFrames()
+		{
+			var clip = _animator.GetCurrentAnimatorClipInfo(0)[0].clip;
+			_frames = clip.frameRate * clip.length;
+			_secondsPerFrame = 1 / _frames;
+		}
+
+		private void SetTimeText()
+		{
+			_timeText.text = _secondsLeft.ToString();
+			if (!_timeText.gameObject.activeSelf)
+			{
+				_timeText.gameObject.SetActive(true);
+			}
+		}
+
+		private void PlayAtFrame(int frame)
+		{
+			var normalizedTime = frame / _frames;
+			_animator.Play("Timer", 0, normalizedTime);
+		}
+
+		private IEnumerator Animation()
+		{
+			_secondsLeft = seconds;
+			SetTimeText();
+			while (_secondsLeft > 0)
+			{
+				for (var i = 0; i < _frames; i++)
+				{
+					PlayAtFrame(i);
+					yield return new WaitForSeconds(_secondsPerFrame);
+				}
+				_secondsLeft--;
+				SetTimeText();
+			}
+			onOutOfTime?.Invoke();
+		}
+	}
+}

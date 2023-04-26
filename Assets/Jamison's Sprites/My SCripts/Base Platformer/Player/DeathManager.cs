93bccc46e71cb6f25d87f9c632f0e03b5b78bf7b
mrKrabsararararararar <118307230+BistroKnight64@users.noreply.github.com>
2023-04-26 13:45:03 -0700

    Itration 0.3

--- Assets/Jamison's Sprites/My SCripts/Base Platformer/Player/DeathManager.cs
+++ Assets/Jamison's Sprites/My SCripts/Base Platformer/Player/DeathManager.cs
@@ -91,6 +91,15 @@ public class DeathManager : MonoBehaviour
 
         }
 
+        //Spike Hazard Check
+        else if(collision.gameObject.CompareTag("Spike Hazard"))
+        {
+            DiedToSpikes = true;
+
+            //Starts cooldown
+            StartCoroutine(SummonCoolDown());
+        }
+
     }
 
     //This brings up the Watch Ad Gameobject after some time.

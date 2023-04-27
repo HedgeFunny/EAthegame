87db73bd0ba5fe3708b30388469af29709e370a9
BoardingAsp642 <letsmakeagame642@gmail.com>
2023-04-27 13:45:49 -0700

    Update to Kerffuffle

--- Assets/Jacob/Scripts/Controllers/HealthSubtract.cs
+++ Assets/Jacob/Scripts/Controllers/HealthSubtract.cs
@@ -10,7 +10,22 @@ namespace Jacob.Scripts.Controllers
 
         private void OnCollisionEnter2D(Collision2D col)
         {
-            if (col.collider.CompareTag("Player"))
+            if (col.collider.CompareTag("Cannon"))
+            {
+                GameManager.Get().Health.SubtractHealth(healthToSubtract);
+            }
+
+            if (col.collider.CompareTag("GoldStorage"))
+            {
+                GameManager.Get().Health.SubtractHealth(healthToSubtract);
+            }
+
+            if (col.collider.CompareTag("ElixirStorage"))
+            {
+                GameManager.Get().Health.SubtractHealth(healthToSubtract);
+            }
+
+            if (col.collider.CompareTag("TownHall"))
             {
                 GameManager.Get().Health.SubtractHealth(healthToSubtract);
             }
